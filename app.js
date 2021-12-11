570b0d210f5e56de695b11c58d15a76eae0325cd
Vishnu Prasad M <vpm735@gmail.com>
2021-12-11 16:32:05 +0530

    process obj

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ console.log(`Hello ${name}`);
 }
 
 greet("Vishnu");
+console.log(`Process Platform ${process.platform}`)
+console.log(`Process Version ${process.version}`)
+console.log(`Process working dir ${process.cwd()}`)

3dbb5f33371500104439a0260fc1ee7df7406374
Vishnu Prasad M <vpm735@gmail.com>
2021-12-11 19:23:45 +0530

    testing chalk

--- app.js
+++ app.js
@@ -9,5 +9,7 @@ console.log(`Process working dir ${process.cwd()}`)
 
 //Console is a part of window object in browser and here in 
 //node it is a part of global object
-var exportedModule = require("./messenger")
-exportedModule.printer("Vishnu from another module")
+var exportedModule = require("./messenger");
+exportedModule.printer("Vishnu from another module");
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
